a29cbe780fe4194c58bb0953d2c28eb0807ee96e
jihirmasl <jihirmas@miuandes.cl>
2023-11-27 23:04:33 -0300

    Update port configuration in main.py

--- SACC/main.py
+++ SACC/main.py
@@ -65,6 +65,8 @@ class Historial(BaseModel):
 if __name__=="__main__":
     models.Base.metadata.create_all(bind=engine)
     # uvicorn.run("app.app:app",port=8000, reload=True)
-    uvicorn.run("app.app:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info", reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.app:app", host="0.0.0.0", port=port, log_level="info", reload=True)
+
 
 
