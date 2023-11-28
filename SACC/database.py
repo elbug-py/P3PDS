41c107c825ec02d35a222eeee48b3e58cca511e1
jihirmasl <jihirmas@miuandes.cl>
2023-11-27 22:37:39 -0300

    Update database URL to use environment variable

--- SACC/database.py
+++ SACC/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
 
-URL_DATABASE = 'postgresql://moki:1331Mati??@localhost:5432/sacc'
+# URL_DATABASE = 'postgresql://moki:1331Mati??@localhost:5432/sacc'
 # URL_DATABASE = 'postgresql://postgres:Admin123@localhost:5432/sacc'
+URL_DATABASE = os.environ.get("DATABASE_URL")
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
