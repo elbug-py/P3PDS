8f11f32c43f1739cb6d8171e8ab7a257211b2c61
vicente romero <vbromero@miuandes.cl>
2023-11-29 17:42:30 -0300

    saving the progress

--- SACC/app/app.py
+++ SACC/app/app.py
@@ -123,6 +123,8 @@ def load_initial_data(db: Session):
         db_locker = models.Locker(state=0, height=40, width=40, depth=20, station_id=1, id=3, personal_id=3)
         db.add(db_locker)
         db.commit()
+
+    
         
         # db_locker = models.Locker(state=0, height=20, width=50, depth=25, station_id=2, id=4, personal_id=1)
         # db.add(db_locker)
@@ -155,6 +157,29 @@ def load_initial_data(db: Session):
         for locker in lockers:
             db_state = models.States(locker_id=locker.id, state=locker.state)
             db.add(db_state)
+        db.commit()
+
+    if not db.query(models.Order).count():
+        db_order = models.Order(name="order1", width=20, height=20, depth=20)
+        db.add(db_order)
+        db_order = models.Order(name="order2", width=30, height=30, depth=30)
+        db.add(db_order)
+        db_order = models.Order(name="order3", width=40, height=40, depth=40)
+        db.add(db_order)
+        db.commit()
+
+    if not db.query(models.Reservation).count():
+        db_reservation = models.Reservation(user_id=3, order_id=1, locker_id=1, locker_personal_id=1, station_id=1, fecha=datetime.now(), estado="activa")
+        db.add(db_reservation)
+        db.commit()
+
+    if not db.query(models.Historial).count():
+        db_historial = models.Historial(reservation_id=3, user_id=1, locker_id=1, station_id=1, fecha=datetime.now(), order_id=1, accion="creacion reserva")
+        db.add(db_historial)
+        db_historial = models.Historial(reservation_id=3, user_id=1, locker_id=2, station_id=1, fecha=datetime.now(), order_id=1, accion="reserva confirmada, medidas correctas")
+        db.add(db_historial)
+        db_historial = models.Historial(reservation_id=3, user_id=1, locker_id=3, station_id=1, fecha=datetime.now(), order_id=1, accion="Listo para retirar")
+        db.add(db_historial)
         db.commit()
     
     
@@ -432,10 +457,14 @@ async def confirm_reservation(reservation: int, db: dp_dependecy):
                     sql_query = text(f'SELECT * FROM "user" WHERE id={1}')
                     result = db.execute(sql_query)
                     operario = result.fetchone()
-                    await send_email_async('Verificar medidas',f'{operario[2]}',
-                            f"debes verificar la reserva {reservation}")
-                    create_record(db, reserva[0], reserva[1], reserva[3], reserva[5], datetime.now(), reserva[2], "No se pudo confirmar por medidas")
-                    #TODO va a entregar y el mqtt cambiar el estado del cajon especifico a reservado
+
+                    try:
+                        await send_email_async('Verificar medidas',f'{operario[2]}',
+                                f"debes verificar la reserva {reservation}")
+                        create_record(db, reserva[0], reserva[1], reserva[3], reserva[5], datetime.now(), reserva[2], "No se pudo confirmar por medidas")
+                        #TODO va a entregar y el mqtt cambiar el estado del cajon especifico a reservado
+                    except Exception as e:
+                        pass
                     
                     if MQTT:
                         mqtt.publish("g1/verification", {"nickname":""}) #publishing mqtt topic
@@ -561,8 +590,11 @@ async def confirm(height: int, width: int, depth: int, reservation: int, db: dp_
                         sql_query = text(f'SELECT * FROM "user" WHERE id={1}')
                         result = db.execute(sql_query)
                         operario = result.fetchone()
-                        await send_email_async('Entregar Product',f'{operario[2]}',
-                                f"Debes entregar en la Estacion G1 en el espacio {locker[1]} el paquete con reservacion {reservation} con el codigo: {locker[6]}")
+                        try:
+                            await send_email_async('Entregar Product',f'{operario[2]}',
+                                    f"Debes entregar en la Estacion G1 en el espacio {locker[1]} el paquete con reservacion {reservation} con el codigo: {locker[6]}")
+                        except Exception as e:
+                            pass
                         return {"message": "Package confirmed"}
                     else:
                         sql_query = text(f"UPDATE locker SET state = 0 WHERE id = {locker[0]}")
@@ -682,8 +714,11 @@ async def load(reservation: int,code: str, db: dp_dependecy):
                     sql_query = text(f'SELECT * FROM "user" WHERE id={3}')
                     result = db.execute(sql_query)
                     cliente = result.fetchone()
-                    await send_email_async('Retirar producto',f'{cliente[2]}',
-                            f"Debes retirar en la Estacion G1 en el espacio {locker[1]} el paquete con reservacion {reservation} con el codigo: {clave}")
+                    try:
+                        await send_email_async('Retirar producto',f'{cliente[2]}',
+                                f"Debes retirar en la Estacion G1 en el espacio {locker[1]} el paquete con reservacion {reservation} con el codigo: {clave}")
+                    except Exception as e:
+                        pass
                     if MQTT:
                         mqtt.publish("g1/verification", {"nickname":""}) #publishing mqtt topic
 
@@ -790,23 +825,52 @@ async def estado_casilleros(request: Request, db: dp_dependecy):
  
     return templates.TemplateResponse("estado_casilleros.html", {"request": request, "saccs": dic})
 
-@app.get('/bitacora/')
-async def bitacora(request: Request, db: dp_dependecy, reservation_id: int = None):
+@app.post('/reservas/{reservation_id}', tags=['RESERVAS'])
+async def reservas( reservation_id: int, request: Request, db: dp_dependecy):
     sql_query = text(f"SELECT * FROM historial WHERE reservation_id = {reservation_id}")
+    result = db.execute(sql_query) 
+    historial = result.fetchall()
+    sql_query = text(f"SELECT * FROM reservation WHERE id = {reservation_id}")
     result = db.execute(sql_query)
-    acciones = result.fetchall()
-    datos = []
-    for i in acciones:
-        sql_query = text(f'SELECT * FROM "user" where id = {i[1]}')
-        result = db.execute(sql_query)
-        usuario = result.fetchone()
-        datos.append((i[0], usuario[1], i[2], i[3], i[4], i[5], i[6], i[7], usuario[3]))
+    actual_reservation = result.fetchone()
+    return templates.TemplateResponse("reservas.html", {"request": request, "historial": historial, "reservation": actual_reservation})
     
-    return templates.TemplateResponse("bitacora.html", {"request": request, "acciones": datos})
 
-@app.get('/reservas/')
-async def reservas(request: Request, db: dp_dependecy):
+# @app.get('/reservas/')
+# async def reservas(request: Request, db: dp_dependecy):
+#     sql_query = text(f"SELECT * FROM reservation")
+#     result = db.execute(sql_query)
+#     reservas = result.fetchall()
+#     return templates.TemplateResponse("reservas.html", {"request": request, "reservas": reservas})
+
+
+
+
+@app.get('/dashboard/')
+async def dashboard(request: Request, db: dp_dependecy):
     sql_query = text(f"SELECT * FROM reservation")
     result = db.execute(sql_query)
     reservas = result.fetchall()
-    return templates.TemplateResponse("reservas.html", {"request": request, "reservas": reservas})
+    sql_query = text(f"SELECT * FROM locker")
+    result = db.execute(sql_query)
+    lockers = result.fetchall()
+    sql_query = text(f"SELECT * FROM station")
+    result = db.execute(sql_query)
+    stations = result.fetchall()
+    sql_query = text(f"SELECT * FROM historial")
+    result = db.execute(sql_query)
+    historial = result.fetchall()
+
+    dates_lockers = {}
+    for registro in historial:
+        if registro[3] not in dates_lockers:
+            dates_lockers[registro[3]] = [registro[5]]
+        else:
+            dates_lockers[registro[3]].append(registro[5])
+
+    
+    oldest_dates = {locker_id: min(dates) for locker_id, dates in dates_lockers.items()}
+    print(oldest_dates)
+    return templates.TemplateResponse("dashboard.html", {"request": request, "reservas": reservas, "lockers": lockers, "stations": stations, "historial": historial, "oldest_dates": oldest_dates})
+
+ 
